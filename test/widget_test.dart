864248ef128546be38bd4251d9df3c5004c4397c
Over_Brave <47111280+OverBrave@users.noreply.github.com>
2023-11-04 01:33:50 +0300

    Initial commit

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:weather/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (final WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(WeatherApp());
+    await tester.pumpWidget(const WeatherApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
