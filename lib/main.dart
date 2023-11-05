6b1199d0d776755e7e682401b0f5f991d30f8b71
Mustafa Yavuz Okumuş <47111280+OverBrave@users.noreply.github.com>
2023-11-05 16:46:34 +0300

    Some fixes

--- lib/main.dart
+++ lib/main.dart
@@ -49,7 +49,7 @@ class WeatherApp extends StatelessWidget {
           '/': (final context) => const WeatherHomePage(),
           '/addCity': (final context) => const AddCity(),
         },
-        scrollBehavior: MaterialScrollBehavior().copyWith(
+        scrollBehavior: const MaterialScrollBehavior().copyWith(
           dragDevices: {
             PointerDeviceKind.mouse,
             PointerDeviceKind.touch,
